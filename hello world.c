2fcdc4b558167c5fa44d098828c4b6c9ca134247
farhana <farhana.s786@gmail.com>
2022-01-10 11:56:00 +0530

    Update hello world.c

--- hello world.c
+++ hello world.c
@@ -2,10 +2,5 @@
 void main() //void is a return type which does not return anything. main() is an inbuilt function where program execution starts.
 {
 	//body of main() . set of statements.
-	int num1,num2,sum; //declaration of integer variable 'num1'
-	printf("Enter two numbers =");
-	scanf("%d%d",&num1,&num2); //& is an address of operator which locates the address of variable 'num1' and scanf() is a inbuilt input function which stores the value in variable
-	sum = num1 + num2; // '=' assignment operator
-	printf("Addition is %d",sum); // %d is format specifier which represent integer values. 
-	                        //semicolon is termination operator which stops the eecution os instruction.
+	printf("Hello World!"); 
 }
